dc5e1eaaf8aa04b217ae47b2b1910d200e161c5a
Alejandro Galue <agalue@opennms.org>
2017-02-20 15:06:51 -0500

    Moving map logic from OnmsMapsService to RegionalStatusPage

--- src/pages/regional-status/regional-status.ts
+++ src/pages/regional-status/regional-status.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { AlertController } from 'ionic-angular';
 
-import { GeolocationQuery, OnmsMapsService } from '../../services/onms-maps';
+import { GeolocationQuery, GeolocationInfo, SeverityInfo, OnmsMapsService } from '../../services/onms-maps';
+
+import * as Leaflet from 'leaflet';
+import 'leaflet.markercluster';
 
 @Component({
   selector: 'page-regional-status',
@@ -11,14 +14,91 @@ export class RegionalStatusPage {
 
   query: GeolocationQuery = new GeolocationQuery();
 
+  private tileLayer: string = 'https://tiles.opennms.org/{z}/{x}/{y}.png';
+  private tileAttribution: string = "Map data &copy; <b>OpenStreetMap</b> contributors under <i>ODbL</i>, <i>CC BY-SA 2.0</i>"
+  private map: Leaflet.Map;
+  private markersGroup: Leaflet.MarkerClusterGroup;
+
+  private mapOptions: Leaflet.MapOptions = {
+    zoom: 1,
+    maxZoom: 15
+  };
+
   constructor(
     private alertCtrl: AlertController,
     private mapService: OnmsMapsService
   ) {}
 
   ionViewDidLoad() {
-    this.mapService.createMap('map', this.query)
-      .catch(error => this.alert('Load Map', error))
+    this.initMap();
+    this.mapService.getGeolocations(this.query)
+      .then(locations => {
+        this.resetMap(locations);
+        this.centerOnMap();
+      })
+      .catch(error => this.alert('Load Map', error));
+  }
+
+  private initMap() {
+    if (this.map) return;
+    this.map = Leaflet.map('map', this.mapOptions);
+    Leaflet.tileLayer(this.tileLayer, { attribution: this.tileAttribution }).addTo(this.map);
+    this.initMarkerGroup();
+  }
+
+  private initMarkerGroup() {
+    this.markersGroup = Leaflet.markerClusterGroup({
+      spiderfyOnMaxZoom: false,
+      showCoverageOnHover: false,
+      zoomToBoundsOnClick: false,
+      iconCreateFunction: (cluster: Leaflet.MarkerCluster) => {
+        let severity = new SeverityInfo();
+        cluster.getAllChildMarkers().forEach(m => {
+          let s = (<Object>m)['data'] as SeverityInfo;
+          if (severity.id < s.id) {
+            severity = s;
+          }
+        });
+        return Leaflet.divIcon({
+          iconSize: Leaflet.point(30, 30),
+          className: `myCluster ${severity.label}_`,
+          html: cluster.getChildCount().toString()
+        }) as Leaflet.Icon;
+      }
+    }).addTo(this.map);
+  }
+
+  private resetMap(locations: GeolocationInfo[]) {
+    this.markersGroup.clearLayers();
+    locations.forEach(location => {
+      if (location.coordinates) {
+        let icon = this.getIcon(location.severityInfo);
+        let latlng = Leaflet.latLng(location.coordinates.latitude, location.coordinates.longitude);
+        let marker = Leaflet.marker(latlng, { icon: icon });
+        (<Object>marker)['data'] = location.severityInfo;
+        this.markersGroup.addLayer(marker);
+      }
+    });
+  }
+
+  private centerOnMap() {
+    if (this.markersGroup.getBounds().isValid()) {
+      this.map.fitBounds(this.markersGroup.getBounds(), {padding: [15, 15]});
+    } else {
+      this.map.setView([34.5133, -94.1629], 1); // center of earth
+    }
+  }
+
+  private getIcon(severityInfo: SeverityInfo) : Leaflet.Icon {
+    let severity = severityInfo && severityInfo.label ? severityInfo.label : 'Normal';
+    return Leaflet.icon({
+        iconUrl: `/assets/images/severity${severity}.png`,
+        iconRetinaUrl: `/assets/images/${severity}@2x.png`,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
   }
 
   private alert(title: string, message: string) {
--- src/services/onms-maps.ts
+++ src/services/onms-maps.ts
@@ -82,96 +82,14 @@ export class GeolocationInfo {
 export class OnmsMapsService {
 
   private retries: number = 3;
-  private tileLayer: string = 'https://tiles.opennms.org/{z}/{x}/{y}.png';
-  private tileAttribution: string = "Map data &copy; <b>OpenStreetMap</b> contributors under <i>ODbL</i>, <i>CC BY-SA 2.0</i>"
-  private map: Leaflet.Map;
-  private markersGroup: Leaflet.MarkerClusterGroup;
-
-  private mapOptions: Leaflet.MapOptions = {
-    zoom: 1,
-    maxZoom: 15
-  };
 
   constructor(private http: HttpService) {}
 
-  createMap(mapId: string, request: GeolocationQuery) : Promise<Leaflet.Map> {
-    return new Promise<Leaflet.Map>((resolve, reject) => {
-      this.initMap(mapId);
-      this.loadGeolocations(request).then(() => {
-        this.centerOnMap();
-        resolve(this.map);
-      });
-    });
-  }
-
-  private initMap(mapId: string) {
-    if (this.map) return;
-    this.map = Leaflet.map(mapId, this.mapOptions);
-    Leaflet.tileLayer(this.tileLayer, { attribution: this.tileAttribution }).addTo(this.map);
-    this.initMarkerGroup();
-  }
-
-  private initMarkerGroup() {
-    this.markersGroup = Leaflet.markerClusterGroup({
-      spiderfyOnMaxZoom: false,
-      showCoverageOnHover: false,
-      zoomToBoundsOnClick: false,
-      iconCreateFunction: (cluster: Leaflet.MarkerCluster) => {
-        let severity = new SeverityInfo();
-        cluster.getAllChildMarkers().forEach(m => {
-          let s = (<Object>m)['data'] as SeverityInfo;
-          if (severity.id < s.id) {
-            severity = s;
-          }
-        });
-        return Leaflet.divIcon({
-          iconSize: Leaflet.point(30, 30),
-          className: `myCluster ${severity.label}_`,
-          html: cluster.getChildCount().toString()
-        }) as Leaflet.Icon;
-      }
-    }).addTo(this.map);
-  }
-
-  private resetMap(locations: GeolocationInfo[]) {
-    this.markersGroup.clearLayers();
-    locations.forEach(location => {
-      if (location.coordinates) {
-        let icon = this.getIcon(location.severityInfo);
-        let latlng = Leaflet.latLng(location.coordinates.latitude, location.coordinates.longitude);
-        let marker = Leaflet.marker(latlng, { icon: icon });
-        (<Object>marker)['data'] = location.severityInfo;
-        this.markersGroup.addLayer(marker);
-      }
-    });
-  }
-
-  private loadGeolocations(request: GeolocationQuery) : Promise<any> {
+  getGeolocations(request: GeolocationQuery) : Promise<GeolocationInfo[]> {
     return this.http.post('/api/v2/geolocation', 'application/json', request)
       .retry(this.retries)
       .map((response: Response) => GeolocationInfo.importLocations(response.json()))
       .toPromise()
-      .then(locations => this.resetMap(locations))
-  }
-
-  private centerOnMap() {
-    if (this.markersGroup.getBounds().isValid()) {
-      this.map.fitBounds(this.markersGroup.getBounds(), {padding: [15, 15]});
-    } else {
-      this.map.setView([34.5133, -94.1629], 1); // center of earth
-    }
-  }
-
-  private getIcon(severityInfo: SeverityInfo) : Leaflet.Icon {
-    let severity = severityInfo && severityInfo.label ? severityInfo.label : 'Normal';
-    return Leaflet.icon({
-        iconUrl: `/assets/images/severity${severity}.png`,
-        iconRetinaUrl: `/assets/images/${severity}@2x.png`,
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    });
   }
 
 }
